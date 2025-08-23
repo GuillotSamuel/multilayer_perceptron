2ff7af82f23b7f3cb9a986cddb06112209e204f0
Samuel Guillot <sguillot@f6r10s5.clusters.42paris.fr>
2025-08-23 18:50:26 +0200

    div

--- Numbers_case/srcs/training/cost.py
+++ Numbers_case/srcs/training/cost.py
@@ -11,7 +11,7 @@ class Cost:
         epsilon = 1e-15
         
         if cost_function == "binaryCrossentropy":
-            cost = -np.mean(np.sum(Y * np.log(A + epsilon), axis=1))  # Used for binary classification, measures log loss
+            cost = -np.mean(Y * np.log(A + epsilon) + (1 - Y) * np.log(1 - A + epsilon))  # Used for binary classification, measures log loss
         elif cost_function == "categoricalCrossentropy":
             cost = -np.mean(np.sum(Y * np.log(A + epsilon), axis=1))  # Used for multi-class classification, generalizes binary cross-entropy
         elif cost_function == "mse":
